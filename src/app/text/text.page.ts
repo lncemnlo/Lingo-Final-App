4684e8aa71d58822bcdfa1bfdf0738084da72abd
lncemnlo <lance.khristian89@gmail.com>
2023-07-12 12:44:43 +0800

    update on text.page.ts

--- src/app/text/text.page.ts
+++ src/app/text/text.page.ts
@@ -24,14 +24,74 @@ export class TextPage {
       "gifName" : "Gulay"
     },
     {
+      "gifURL" :  "./assets/iyak.gif",
+      "gifName" : "Iyak"
+    },
+    {
       "gifURL" : "./assets/hello.gif",
       "gifName" : "Hello"
     },
     {
-      "gifURL" : "./assets/iyak.gif",
-      "gifName" : "Iyak"
-    }
-    
+      "gifURL" : "./assets/kaibigan.gif",
+      "gifName" : "Kaibigan"
+    },
+    {
+      "gifURL" : "./assets/lalaki.gif",
+      "gifName" : "Lalaki"
+    },
+    {
+      "gifURL" : "./assets/lola.gif",
+      "gifName" : "Lola"
+    },
+    {
+      "gifURL" : "./assets/lolo.gif",
+      "gifName" : "Lolo"
+    },
+    {
+      "gifURL" : "./assets/magandang gabi.gif",
+      "gifName" : "Magandang Gabi"
+    },
+    {
+      "gifURL" : "./assets/magandang tanghali.gif",
+      "gifName" : "Magandang Tanghali"
+    },
+    {
+      "gifURL" : "./assets/magandang umaga.gif",
+      "gifName" : "Magandang Umaga"
+    },
+    {
+      "gifURL" : "./assets/magkano.gif",
+      "gifName" : "Magkano"
+    },
+    {
+      "gifURL" : "./assets/maligayang kaarawan.gif",
+      "gifName" : "Maligayang Kaarawan"
+    },
+    {
+      "gifURL" : "./assets/masaya.gif",
+      "gifName" : "Masaya"
+    },
+    {
+      "gifURL" : "./assets/paalam.gif",
+      "gifName" : "Paalam"
+    },
+    {
+      "gifURL" : "./assets/pinsan.gif",
+      "gifName" : "Pinsan"
+    },
+    {
+      "gifURL" : "./assets/prutas.gif",
+      "gifName" : "Prutas"
+    },
+    {
+      "gifURL" : "./assets/salamat.gif",
+      "gifName" : "Salamat"
+    },
+    {
+      "gifURL" : "./assets/sanggol.gif",
+      "gifName" : "Sanggol"
+    },
+
     
   ]
 
